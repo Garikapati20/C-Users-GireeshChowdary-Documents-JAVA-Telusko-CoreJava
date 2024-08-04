3b033492e4d8e2b18791e6e68dcfb07937a3d79d
Gireesh <Garikapati20@gmail.com>
2024-08-04 15:21:26 -0400

    Example Problems

--- src/main/java/org/example/FibonacciNumber.java
+++ src/main/java/org/example/FibonacciNumber.java
@@ -2,15 +2,12 @@ package org.example;
 
 public class FibonacciNumber {
 
-    // Removed redundant variables and added a method for calculating Fibonacci number
     public void calculateFibonacci(int m, int n) {
         System.out.print(m + " " + n + " ");
         for (int i = 0; i < 10; i++) {
-
             int sum = m + n;
             m = n;
             n = sum;
-
             System.out.print(sum + " ");
         }
     }
